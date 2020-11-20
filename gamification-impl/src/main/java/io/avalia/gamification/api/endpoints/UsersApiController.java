d452a387ddbbd96c32bec6bab3c42350adadd21f
Baptiste Hardrick <batach31@hotmail.com>
2020-11-20 10:01:00 +0100

    now users is a controller

--- gamification-impl/src/main/java/io/avalia/gamification/api/endpoints/UsersApiController.java
+++ gamification-impl/src/main/java/io/avalia/gamification/api/endpoints/UsersApiController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,6 +23,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 public class UsersApiController implements UsersApi {
     @Autowired
     UserRepository userRepository;
