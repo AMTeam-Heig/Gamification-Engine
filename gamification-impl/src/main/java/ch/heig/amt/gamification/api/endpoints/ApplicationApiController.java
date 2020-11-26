10fd5c523a6fa67ddffb34a2d539fc7d9d6ae7f9
Baptiste Hardrick <batach31@hotmail.com>
2020-11-26 15:11:12 +0100

    pour elo

--- gamification-impl/src/main/java/ch/heig/amt/gamification/api/endpoints/ApplicationApiController.java
+++ gamification-impl/src/main/java/ch/heig/amt/gamification/api/endpoints/ApplicationApiController.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class ApplicationApiController implements ApplicationApi {
@@ -30,10 +32,10 @@ public class ApplicationApiController implements ApplicationApi {
         return ResponseEntity.ok(application);
     }
 
-    public ResponseEntity<Application> getApplication(String xApiKey) {
-        Application application = new Application();
-        application.setApiKey(xApiKey);
-        application.setName(applicationRepository.findByApiKey(xApiKey).getName());
-        return ResponseEntity.ok(application);
+    public ResponseEntity<List<Application>> getApplication(String xApiKey) {
+        List<Application> applications = new LinkedList<>();
+        applications.get(0).setApiKey(xApiKey);
+        applications.get(0).setName(applicationRepository.findByApiKey(xApiKey).getName());
+        return ResponseEntity.ok(applications);
     }
 }
