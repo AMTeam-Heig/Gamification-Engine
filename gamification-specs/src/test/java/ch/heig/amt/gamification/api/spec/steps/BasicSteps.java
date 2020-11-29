8d1287bd0bae1faebab1f4b91e9e33e243b9b55b
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-11-29 19:58:54 +0100

    Update BasicSteps.java

--- gamification-specs/src/test/java/ch/heig/amt/gamification/api/spec/steps/BasicSteps.java
+++ gamification-specs/src/test/java/ch/heig/amt/gamification/api/spec/steps/BasicSteps.java
@@ -1,19 +1,18 @@
 package ch.heig.amt.gamification.api.spec.steps;
 
-import ch.heig.amt.gamification.api.*;
-
-import ch.heig.amt.gamification.api.dto.NewApplication;
-import ch.heig.amt.gamification.api.spec.helpers.Environment;
 import ch.heig.amt.gamification.ApiException;
 import ch.heig.amt.gamification.ApiResponse;
-import ch.heig.amt.gamification.api.dto.Application;
-import ch.heig.amt.gamification.api.dto.Badge;
+import ch.heig.amt.gamification.api.DefaultApi;
+import ch.heig.amt.gamification.api.dto.*;
+import ch.heig.amt.gamification.api.spec.helpers.Environment;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -23,7 +22,8 @@ public class BasicSteps {
     private Environment environment;
     private DefaultApi api;
 
-    private NewApplication application;
+    private NewApplication newApplication;
+    private Application application;
     private ApiResponse lastApiResponse;
     private ApiException lastApiException;
     private boolean lastApiCallThrewException;
@@ -33,10 +33,15 @@ public class BasicSteps {
 
     private String lastReceivedApiKey = "";
 
-    Badge badge;
 
+    private Event event;
+
+    private String testAppApiKey;
+
+
+    Badge badge;
     private Badge lastReceivedBadge;
-    private Application lastReceivedApplication;
+
 
     public BasicSteps(Environment environment) {
         this.environment = environment;
@@ -48,46 +53,134 @@ public class BasicSteps {
         assertNotNull(api);
     }
 
-    @Given("I have an application payload")
-    public void i_have_an_application_payload() {
-        application = new ch.heig.amt.gamification.api.dto.NewApplication().name("New Application");
-    }
-    @When("^I POST the application payload to the /applications endpoint$")
-    public void i_POST_the_application_payload_to_the_application_endpoint() throws Throwable {
+    @Given("I have a Gamification client")
+    public void iHaveAGamificationClient() {
+        String generatedName = RandomStringUtils.randomAlphanumeric(10);
+        NewApplication testApp = new ch.heig.amt.gamification.api.dto.NewApplication().name(generatedName);
         try {
-            lastApiResponse = api.createApplicationWithHttpInfo(application);
-            processApiResponse(lastApiResponse);
+            api.createApplication(testApp);
+            testAppApiKey = api.getApplication(generatedName).getApiKey();
         } catch (ApiException e) {
             processApiException(e);
         }
     }
+
     @Then("I receive a {int} status code")
     public void i_receive_a_status_code(int expectedStatusCode) throws Throwable {
         assertEquals(expectedStatusCode, lastStatusCode);
     }
 
-    @Then("I receive a payload that has an api key")
-    public void i_receive_a_payload_that_has_an_api_key() {
-       // Application a = (Application)lastApiResponse.getData();
+
+    // ===================================== Application =========================================
+    // ==========================================================================================
+    @Given("I have an application payload {string}")
+    public void i_have_an_application_payload(String name) {
+        newApplication = new ch.heig.amt.gamification.api.dto.NewApplication().name(name);
+    }
+
+    @When("^I POST the application payload to the /applications endpoint$")
+    public void i_POST_the_application_payload_to_the_application_endpoint() throws Throwable {
+        try {
+            lastApiResponse = api.createApplicationWithHttpInfo(newApplication);
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
     }
 
     @When("^I send a GET to the /applications endpoint$")
     public void i_send_a_get_to_the_applications_endpoint() {
-      try {
-          lastApiResponse = api.getApplicationsWithHttpInfo();
-          processApiResponse(lastApiResponse);
-      } catch (ApiException e) {
-          processApiException(e);
-      }
+        try {
+            lastApiResponse = api.getApplicationsWithHttpInfo();
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
     }
 
+    @And("I see my new application {string} in the list")
+    public void iSeeMyNewApplicationInTheList(String name) {
+        boolean exists = false;
+        ArrayList<Application> list = (ArrayList) lastApiResponse.getData();
+        for (Application a : list)
+            if (a.getName().equals(name)) {
+                exists = true;
+                break;
+            }
+        assertTrue(exists);
+    }
+
+
+    @When("I send a GET to the /applications endpoint with application {string} name")
+    public void iSendAGETToTheApplicationsEndpointWithApplicationName(String name) {
+        try {
+            lastApiResponse = api.getApplicationWithHttpInfo(name);
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
+    }
+
+    @Then("I receive a payload of the application {string}")
+    public void i_receive_a_payload_of_the_application(String name) {
+        Application responseApplication = (Application) lastApiResponse.getData();
+        assertEquals(responseApplication.getName(), name);
+    }
+    // ==========================================================================================
+    // ==========================================================================================
+
+    // ========================================= USER ===========================================
+    // ========================================================================================
+    @When("I POST the user payload : {string} {int} {string} {int} {string} to the \\/users endpoint")
+    public void i_post_the_user_payload_to_the_users_endpoint(String birthdate, Integer points, String reputation, Integer role, String username) {
+        User user = new User().birthdate(LocalDate.parse(birthdate))
+                .points(points)
+                .reputation(reputation)
+                .role(role)
+                .username(username);
+
+        try {
+            lastApiResponse = api.createUserWithHttpInfo(testAppApiKey, user);
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
+    }
+    @When("I send a GET to the \\/users endpoint")
+    public void iSendAGETToTheUsersEndpoint() {
+        try {
+            lastApiResponse = api.getUsersWithHttpInfo(testAppApiKey);
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
+    }
+
+    @When("I send a GET to the \\/users endpoint with username {string}")
+    public void iSendAGETToTheUsersEndpointWithUsername(String username) {
+        try {
+            lastApiResponse = api.getUserWithHttpInfo(testAppApiKey, username);
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
+    }
+
+    @And("I receive a payload of user {string}")
+    public void iReceiveAPayloadOfUser(String username) {
+        // verification seulement du username
+        
+        User responseUser = (User)lastApiResponse.getData();
+        assertEquals(responseUser.getUsername(), username);
+    }
+    // ==========================================================================================
+    // ==========================================================================================
     protected void processApiResponse(ApiResponse apiResponse) {
         lastApiResponse = apiResponse;
         lastApiCallThrewException = false;
         lastApiException = null;
-        lastReceivedApiKey = (String) apiResponse.getHeaders().get("apiKey");
         lastStatusCode = lastApiResponse.getStatusCode();
-        List<String> locationHeaderValues = (List<String>)lastApiResponse.getHeaders().get("Location");
+        List<String> locationHeaderValues = (List<String>) lastApiResponse.getHeaders().get("Location");
         lastReceivedLocationHeader = locationHeaderValues != null ? locationHeaderValues.get(0) : null;
     }
 
@@ -97,14 +190,36 @@ public class BasicSteps {
         lastApiException = apiException;
         lastStatusCode = lastApiException.getCode();
     }
-/*
+
+    @Given("I have an event payload with name {string} and {int} number of points for user {string}")
+    public void iHaveAnEventPayload(String name, int points, String username) {
+        event = new Event().name(name)
+                .points(points)
+                .username(username);
+    }
+
+    @When("I POST the event payload to the \\/events endpoint")
+    public void iPOSTTheEventPayloadToTheEventsEndpoint() {
+        try {
+            lastApiResponse = api.createEventWithHttpInfo("cpicpi", event);
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
+    }
+
+    @When("I send a GET to the \\/events endpoint")
+    public void iSendAGETToTheEventsEndpoint() {
+    }
+
     @Given("I have a badge payload")
     public void i_have_a_badge_payload() throws Throwable {
         badge = new ch.heig.amt.gamification.api.dto.Badge()
-                .level("Master")
+                .name("Master")
                 .description("Post 100 answers to questions")
                 .obtainedOnDate(LocalDate.now());
     }
+
 
     @When("^I POST the badge payload to the /badges endpoint$")
     public void i_POST_the_badge_payload_to_the_badges_endpoint() throws Throwable {
@@ -134,20 +249,29 @@ public class BasicSteps {
 
 
 
-    @When("^I send a GET to the /application endpoint$")
-    public void iSendAGETToTheApplicationEndpoint() {
-        try {
-            lastApiResponse = api.getApplicationsWithHttpInfo(lastReceivedApiKey);
-            processApiResponse(lastApiResponse);
-        } catch (ApiException e) {
-            processApiException(e);
-        }
-    }
 
-    @And("I receive a payload that is the same as the event payload")
-    public void iReceiveAPayloadThatIsTheSameAsTheApplicationPayload() {
-        assertEquals(application, lastReceivedApplication);
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
 
     @And("I receive an event payload that is null")
     public void iReceiveAnApplicationPayloadThatIsNull() {
@@ -186,17 +310,7 @@ public class BasicSteps {
     public void iReceiveAPayloadThatHasAnApiKey() {
     }
 
-    @When("I send a GET to the URL in the application location header")
-    public void iSendAGETToTheURLInTheApplicationLocationHeader() {
-        Integer id = Integer.parseInt(lastReceivedLocationHeader.substring(lastReceivedLocationHeader.lastIndexOf('/') + 1));
-        try {
-            lastApiResponse = api.getApplicationsWithHttpInfo(lastReceivedApiKey);
-            processApiResponse(lastApiResponse);
-            lastReceivedApplication = (Application) lastApiResponse.getData();
-        } catch (ApiException e) {
-            processApiException(e);
-        }
-    }
+
 
      */
 
